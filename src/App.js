94f71d5eb69f4015c09374ad0455970148afda06
Archie Godfrey <archiegod@gmail.com>
2020-06-30 18:53:43 +0100

    Corrected paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './views/dashboard';
+import Dashboard from './views/Dashboard';
 
 function App() {
   return (
