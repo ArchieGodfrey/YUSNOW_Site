830647965c8d70b2419b49def78636bda6c382f1
Archie Godfrey <archiegod@gmail.com>
2020-06-30 19:38:50 +0100

    Added comments

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+// So this is the same as the 'Dashboard' function except it uses ES6 syntax (which is just the latest)
+
+/*
+    OLD SYNTAX BUT STILL PERFECTLY VALID
+    function Input(placeholder) = { FUNCTION }
+*/
+
 const Input = ({ placeholder }) => (
     <input placeholder={placeholder} />
 );
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,16 +1,22 @@
+// We 'import' react so it can use it
 import React from 'react';
+// We 'import' the 'Dashboard.css' so we can style the page
 import './Dashboard.css';
+// We 'import' the 'Input' component so we can use it on the page
 import Input from '../components/input';
 
+// So this a function with name 'Dashboard' and it returns a div and an Input
 function Dashboard() {
   return (
   <div id="container">
     <div className="template">
         BASIC BITCH TEMPLATE
     </div>
-    <Input />
+    {/* This is how we use components */}
+    <Input placeholder="This is called a prop but its basically a parameter" />
   </div>
   );
 }
 
+// We 'export' the function so that it can be 'import'ed in the 'App.js' file
 export default Dashboard;
