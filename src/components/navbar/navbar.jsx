52f780d4c27e159309dc289716620ea9b489d5d9
Oliver <ojd517@york.ac.uk>
2020-07-01 18:01:38 +0100

    Hover-background

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,7 +4,18 @@ import "./navbar.css";
 function Navbar() {
     return(
         <div className="navbar">
-            <img src={require('../../assets/YUSNOW_LOGO.png')} className="image"/>
+            <img 
+                src={require('../../assets/YUSNOW_LOGO.png')} 
+                className="image"
+            />
+            <div className="col">
+                <div className="title">
+                    {"yusnow".toUpperCase()}
+                </div>
+                <div className="slogon">
+                    {"York University Ski and Snowboard Society"}
+                </div>
+            </div>
         </div>
     )
 }
