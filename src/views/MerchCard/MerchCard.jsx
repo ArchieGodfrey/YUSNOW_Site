a3233d29a7f86ad069a635e9d248b8dc9a31f7cd
Archie <archiegod@gmail.com>
2020-07-09 14:23:18 +0100

    Corrected sizing

--- src/views/MerchCard/MerchCard.jsx
+++ src/views/MerchCard/MerchCard.jsx
@@ -4,17 +4,19 @@ import './MerchCard.css';
 function MerchCard() {
   return (
     <div className="MerchRoot">
+      <div className="container">
         <div className="header">
-            MERCH
+              MERCH
+          </div>
+          <div className="bar" />
+          <div className="row">
+              <div className="image">This is an image</div>
+              <div>
+                  <div className="text">This is a t shirt</div>
+                  <div className="text">This is its price</div>
+              </div>
+            </div>
         </div>
-        <div className="bar" />
-        <div className="row">
-            <div className="image">This is an image</div>
-            <div>
-                <div className="text">This is a t shirt</div>
-                <div className="text">This is its price</div>
-            </div>
-            </div>
     </div>
   );
 }
