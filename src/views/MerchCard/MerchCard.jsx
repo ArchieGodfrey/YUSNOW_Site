dbe5d0e819dc8cf3bfe8edd18e1351a7835691b4
Archie Godfrey <archiegod@gmail.com>
2020-07-13 17:39:59 +0100

    Fixed flow

--- src/views/MerchCard/MerchCard.jsx
+++ src/views/MerchCard/MerchCard.jsx
@@ -6,22 +6,18 @@ function MerchCard({ title, text, imageComponent, right, id }) {
     <div className={right ? "MerchRoot justifyContent" : "MerchRoot"} id={id}>
       <div className={right ? "rowReverse" : "row"}>
         <div className={right ? "container alignItems" : "container"}>
-          <div className={right ? "MerchRoot justifyContent" : "MerchRoot"}>
-            <div className={right ? "rowReverse" : "row"}>
-              <div className="header">
-                    {title}
-                </div>
-                <div className="bar" />
-                <div className="row">
-                    {imageComponent && (
-                    imageComponent
-                    )}
-                    <div>
-                        <div className="text">{text}</div>
-                    </div>
-                  </div>
-              </div>
+          <div className="header">
+                {title}
             </div>
+            <div className="bar" />
+            <div className="row">
+                {imageComponent && (
+                imageComponent
+                )}
+                <div>
+                    <div className="text">{text}</div>
+                </div>
+              </div>
           </div>
         <div className={right ? "triangleLeft" : "triangleRight"} />
       </div>
