71efaba2509c7af8a5a70ad50bb3e0fa5ef3f083
Archie <archiegod@gmail.com>
2020-07-15 18:16:36 +0100

    fixed container

--- src/components/emailCard/emailCard.jsx
+++ src/components/emailCard/emailCard.jsx
@@ -7,52 +7,36 @@ function EmailCard() {
             <div className="container">
                 <div className="header">CONTACT</div>
                 <div className="bar"/>
-                    <div style={{display:"flex"}}>
-                        <div style={{display:"flex", flexDirection:"row"}}>
+                    <div class="row">
+                        <div style={{ display:"flex", flex: 2 }}>
                             <div className="contents">
-                                <form action="mailto:oliver.driver100@gmail.com" method="post" enctype="text/plain">
-                                    <div>
-                                    <input className="input" type="text" name="name" size="50" placeholder="Name"/>
-                                    </div>
-                                    <div>
-                                    <input className="input" type="email" name="email" size="50" placeholder="Email"/>
-                                    </div>
-                                    
-                                    <div>
-                                    <textarea className="input" type="text" name="name" rows="10" cols="100" placeholder="Get into contact with us here..."/>
-                                    </div>
-                                    
-                                    <button className="buttonMargin">Send</button>
-                                </form>
+                              <form action="mailto:oliver.driver100@gmail.com" method="post" enctype="text/plain">
+                                <input className="input" type="text" name="name" placeholder="Name"/>
+                                <input className="input" type="email" name="email" placeholder="Email"/>
+                                <textarea className="input" type="text" name="name" rows="10" placeholder="Get into contact with us here..."/>
+                                <button className="buttonMargin">Send</button>
+                              </form>
                             </div>
                         </div>
                         <div className="contents">
                             <div className="title">Our infomation</div>
                             <div className="text">snow@yusu.org</div>
-                            <div style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
-                                <a href="https://www.facebook.com/YUsnow/" target="_blank" rel="noopener noreferrer">
-                                    <img 
-                                        src={require('../../assets/social-media-logos/facebook_white_logo.png')} 
-                                        className="image"
-                                        alt="Logo"
-                                    />
-                                </a>
-                                <a href="https://www.facebook.com/YUsnow/" target="_blank" rel="noopener noreferrer">
-                                    <div classname="text">www.facebook.com/YUsnow/</div>
-                                </a>
-                            </div>
-                            <div style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
-                                <a href="https://www.instagram.com/yorkunisnow/" target="_blank" rel="noopener noreferrer">
-                                    <img 
-                                        src={require('../../assets/social-media-logos/instagram_white_logo.png')} 
-                                        className="image"
-                                        alt="Logo"
-                                    />
-                                </a>
-                                <a href="https://www.instagram.com/yorkunisnow/" target="_blank" rel="noopener noreferrer">
-                                    <div classname="text">www.instagram.com/yorkunisnow/</div>
-                                </a>
-                            </div>
+                            <a href="https://www.facebook.com/YUsnow/" target="_blank" rel="noopener noreferrer">
+                                <img 
+                                    src={require('../../assets/social-media-logos/facebook_white_logo.png')} 
+                                    className="image"
+                                    alt="Logo"
+                                />
+                                <div classname="text">www.facebook.com/YUsnow/</div>
+                            </a>
+                            <a href="https://www.instagram.com/yorkunisnow/" target="_blank" rel="noopener noreferrer">
+                                <img 
+                                    src={require('../../assets/social-media-logos/instagram_white_logo.png')} 
+                                    className="image"
+                                    alt="Logo"
+                                />
+                                <div classname="text">www.instagram.com/yorkunisnow/</div>
+                            </a>
                         </div> 
                     </div>
                 </div>
