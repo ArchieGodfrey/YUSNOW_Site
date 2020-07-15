d1cac486f92d2da4f08253e7f926f65e8e4b5829
Archie Godfrey <archiegod@gmail.com>
2020-07-15 21:31:58 +0100

    adjusted values

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,7 +3,7 @@ import "./navbar.css";
 
 function Navbar({ scrollToRef, scrollPosition}) {
     return(
-        <div className="navbar" style={scrollPosition > 100 ? { backgroundColor: `rgba(30,30,30,${(scrollPosition - 100) * 0.0015})`} : {}}>
+        <div className="navbar" style={scrollPosition > 300 ? { backgroundColor: `rgba(30,30,30,${(scrollPosition - 300) * 0.002})`} : {}}>
             <div style={{display:"flex"}}>
                 <img 
                     src={require('../../assets/YUSNOW_LOGO.png')} 
@@ -21,8 +21,8 @@ function Navbar({ scrollToRef, scrollPosition}) {
                     </div>
                 </div>
             </div>
-            {scrollPosition > 400 && (
-              <div className="buttonBar" style={{ opacity: (scrollPosition - 100) * 0.0015 }}>
+            {scrollPosition > 300 && (
+              <div className="buttonBar" style={{ opacity: (scrollPosition - 300) * 0.002 }}>
                   <div class="button" onClick={() => scrollToRef('about')}>
                         <a class="custom-underline">About</a>
                   </div>
