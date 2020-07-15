265e6d6141bf42cdc28590c19f2983cd9e7a6b23
Archie <archiegod@gmail.com>
2020-07-15 15:23:14 +0100

    Fixed merch cards

--- src/views/MerchCard/MerchCard.jsx
+++ src/views/MerchCard/MerchCard.jsx
@@ -2,47 +2,43 @@ import React from 'react';
 import './MerchCard.css';
 import ImageWithButtons from '../../components/ImageWithButtons/ImageWithButtons';
 
-function MerchCard({ title, text, imageComponent, right, id, info}) {
+function MerchCard({ title, text, right, id, info}) {
 const [slideIndex, setSlideIndex]=React.useState(0);
 //const [slideIndexAbout, setSlideIndexAbout]=React.useState(0);
   return (
     <div className={right ? "MerchRoot justifyContent" : "MerchRoot"} id={id}>
       <div className={right ? "rowReverse" : "row"}>
         <div className={right ? "container alignItems" : "container"}>
-          <div className="header">
-                {title}
-            </div>
-            <div className="bar" />
-            <div className="row">
-                {imageComponent && (
-                imageComponent
-                )}
-                <div>
-                    <div className="text">{text}</div>
-                    <div className={right ? "aboutContainer" : "merchContainer"}>
-                    <ImageWithButtons
-                      array={info}
-                      slideIndex={slideIndex}
-                      setSlideIndex={setSlideIndex}
-                      slide={
-                        <div className={right ? "rotateContainerRight" : "rotateContainerLeft"}>
-                          <img
-                              src={info[slideIndex].image}
-                              className="image"
-                              alt="Logo"
-                          />
-                          <div className="name">
-                            {info[slideIndex].name}
-                          </div>
-                          <div className="description">
-                            {info[slideIndex].description}
-                          </div>
-                        </div>
-                      }/>
+          <div className="heading">{title}</div>
+          <div className="bar" />
+          <div className="row">
+            <div className="text">{text}</div>
+            <div className={right ? "rotateContainerRight" : "rotateContainerLeft"}>
+              <ImageWithButtons
+                array={info}
+                slideIndex={slideIndex}
+                setSlideIndex={setSlideIndex}
+                slide={
+                  <div className="imageWrapper">
+                    <div className="imageWindow">
+                      <img
+                          src={info[slideIndex].image}
+                          className="image"
+                          alt="Logo"
+                      />
                     </div>
-                </div>
-              </div>
+                    <div className="name">
+                      {info[slideIndex].name}
+                    </div>
+                    <div className="description">
+                      {info[slideIndex].description}
+                    </div>
+                  </div>
+                }
+              />
+            </div>
           </div>
+        </div>
         <div className={right ? "triangleLeft" : "triangleRight"} />
       </div>
     </div>
